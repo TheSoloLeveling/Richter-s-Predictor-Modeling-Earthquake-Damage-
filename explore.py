106152a482205d750c2295a4ab253e9218208a04
bouziane yassine <62574566+TheSoloLeveling@users.noreply.github.com>
2022-05-15 16:05:35 +0100

    update rrport

--- explore.py
+++ explore.py
@@ -20,4 +20,45 @@ label=pd.read_csv('Data/train_labels.csv')
 # Add Target variable to training data
 data['damage']=label['damage_grade']
 
-data.info()
+"""""
+# Check correlation of all the columns to see if something stands-out explicitly
+corr=data.corr()
+plt.subplots(figsize=(10,10))
+
+sns.heatmap(corr, xticklabels=True,yticklabels=True,  vmin=0, vmax=1,
+    cmap=sns.diverging_palette(220, 20, n=100),
+    square=True)
+
+
+data['damage'].value_counts().sort_index().plot(kind='bar')
+plt.xlabel('Damage Grade')
+plt.ylabel('No. of records')
+plt.title('Target distribution')
+
+
+data['count_floors_pre_eq'].value_counts().sort_index().plot(kind='bar')
+plt.xlabel('Damage Grade')
+plt.ylabel('No. of records')
+plt.title('Distribution of count_floors_pre_eq variable')
+
+
+data['age'].value_counts().sort_index().plot.bar()
+
+sns.countplot(x='height_percentage',data=data, hue='damage');
+
+
+a=data.groupby(['height_percentage','damage']).size().reset_index()
+a.head()
+b=a.pivot(index='height_percentage',columns='damage',values=0).reset_index()
+print(b.head())
+b.set_index('height_percentage',inplace=True)
+b.plot(kind='bar',stacked=True,title='Type of damage by height percentage of buildings');
+"""""
+
+
+data['count_families'].value_counts().sort_index().plot(kind='bar')
+plt.xlabel('Damage Grade')
+plt.ylabel('No. of records')
+plt.title('Distribution of count_families variable')
+plt.show()
+
---
+++ model.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.model_selection import cross_validate,KFold, RandomizedSearchCV
+from sklearn.model_selection import train_test_split
+
+from sklearn.utils import resample
+
+from xgboost import XGBClassifier
+
+# Pre set values for max cols and chart size
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+
+plt.rcParams["figure.figsize"] = (15,5)
+
+# Read training data
+data=pd.read_csv('Data/train_values.csv')
+data.head()
+
+# Read table with target variable
+label=pd.read_csv('Data/train_labels.csv')
+label.head()
+
+# Add Target variable to training data
+data['damage']=label['damage_grade']
+
+# Check distribution of target variable to find out if there is class imbaance problem
+data['damage'].value_counts()
+
+# Define X and y variables
+X=pd.get_dummies(data.loc[:,:'has_secondary_use_other'])
+y=data['damage'].astype(int)
+
+# Parameters for XGboost
+n_jobs=[-1]
+n_estimators=np.arange(100,1000,100)
+learning_rate=[0.03,0.01,0.1]
+max_depth=np.arange(10,100,15)
+
+# Param grid for Xgboost
+param_grid={'n_jobs':n_jobs,
+            'n_estimators':n_estimators,
+            'max_depth':max_depth,
+            'learning_rate':learning_rate
+           }
+
+# Fit scaled traing data on XGboost Classifier- This step took 17 hours to complete
+clf=XGBClassifier()
+
+kf=KFold(n_splits=2,shuffle=True)
+
+rs=RandomizedSearchCV(clf,param_distributions=param_grid,cv=kf,scoring='f1_micro')
+
+rs.fit(X,y)
+
+
